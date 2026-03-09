f6997eb80465c3a5a02911dcd95575a3ce47d47c
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:02:56 +0300

    feat: contentPiecesParMarque

---
+++ src/java/classes/PieceParMarque.java
@@ -0,0 +1,21 @@
+public class PieceParMarque {
+
+    private String marque;
+    private int nbrePiece;
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public void setNbrePiece(int nbrePiece) {
+        this.nbrePiece = nbrePiece;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public int getNbrePiece() {
+        return nbrePiece;
+    }
+}
