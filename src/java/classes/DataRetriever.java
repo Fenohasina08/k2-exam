12a2171fa2fb3b67fbc06619fc5d692a1f782c1b
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:08:14 +0300

    feat: reponse1-a

--- src/java/classes/DataRetriever.java
+++ src/java/classes/DataRetriever.java
@@ -37,33 +37,32 @@ public class DataRetriever {
     }
     }
 
-    public List<PieceParMarque> getPiecesParMarque(Connection connection)  {
+public PieceParModele findPiecesParModele() {
+    PieceParModele piece = new PieceParModele();
 
-        List<PieceParMarque> resultat = new ArrayList<>();
+    try (Connection connection = dbConnection.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(
+                 "SELECT " +
+                         "SUM(CASE WHEN Modele_voiture.modele = 'GETZ' THEN Vente.quantite ELSE 0 END) AS GETZ, " +
+                         "SUM(CASE WHEN Modele_voiture.modele = 'PRIDE' THEN Vente.quantite ELSE 0 END) AS PRIDE, " +
+                         "SUM(CASE WHEN Modele_voiture.modele = 'LACETTI' THEN Vente.quantite ELSE 0 END) AS LACETTI " +
+                         "FROM Vente " +
+                         "JOIN Piece_auto ON Vente.id_piece_auto = Piece_auto.id " +
+                         "JOIN Modele_voiture ON Piece_auto.id_modele_voiture = Modele_voiture.id"
+         );
+         ResultSet resultSet = preparedStatement.executeQuery()) {
 
-        String sql = """
-        SELECT Modele_voiture.marque,
-               SUM(Vente.quantite) AS nbre_piece
-        FROM Vente
-        JOIN Piece_auto ON Vente.id_piece_auto = Piece_auto.id
-        JOIN Modele_voiture ON Piece_auto.id_modele_voiture = Modele_voiture.id
-        GROUP BY Modele_voiture.marque
-    """;
-
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-
-        while (resultSet.next()) {
-
-            PieceParMarque piece = new PieceParMarque();
-
-            piece.setMarque(resultSet.getString("marque"));
-            piece.setNbrePiece(resultSet.getInt("nbre_piece"));
-
-            resultat.add(piece);
+        if (resultSet.next()) {
+            piece.setGetz(resultSet.getInt("GETZ"));
+            piece.setPride(resultSet.getInt("PRIDE"));
+            piece.setLacetti(resultSet.getInt("LACETTI"));
         }
 
-        return resultat;
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
     }
+
+    return piece;
+}
 }
 
