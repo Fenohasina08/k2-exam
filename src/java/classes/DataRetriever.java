92448849eac959ba509f57c6ec6b93dd71af088a
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-09 22:05:14 +0300

    feat: reponse1-a

--- src/java/classes/DataRetriever.java
+++ src/java/classes/DataRetriever.java
@@ -9,6 +9,33 @@ public class DataRetriever {
         this.dbConnection = dbConnection;
     }
 
+    public List<PieceParMarque> getPiecesParMarque(Connection connection)  {
 
+        List<PieceParMarque> resultat = new ArrayList<>();
+
+        String sql = """
+        SELECT Modele_voiture.marque,
+               SUM(Vente.quantite) AS nbre_piece
+        FROM Vente
+        JOIN Piece_auto ON Vente.id_piece_auto = Piece_auto.id
+        JOIN Modele_voiture ON Piece_auto.id_modele_voiture = Modele_voiture.id
+        GROUP BY Modele_voiture.marque
+    """;
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+
+            PieceParMarque piece = new PieceParMarque();
+
+            piece.setMarque(resultSet.getString("marque"));
+            piece.setNbrePiece(resultSet.getInt("nbre_piece"));
+
+            resultat.add(piece);
+        }
+
+        return resultat;
+    }
 }
 
